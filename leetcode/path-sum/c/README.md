14ce574e62dad014ee4fc49cee3a117deee504e9
Sewon Min <86864525+givemethatsewon@users.noreply.github.com>
2024-05-29 10:13:18 +0900

    [leetcode] path-sum (Time: 3ms, Memory: 7MB) - by Code-Vault

---
+++ leetcode/path-sum/c/README.md
@@ -0,0 +1,8 @@
+# Path Sum
+
+[문제 출처](https://leetcode.com/problems/path-sum)
+
+## 성능
+
+- Time: 3ms
+- Memory: 7MB
---
+++ leetcode/path-sum/c/path-sum.c
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    if (!root) return false;
+
+    targetSum -= root->val;
+    
+    if (!root->left && !root->right) {
+        if (targetSum == 0) return true;
+        return false;
+    }
+
+    if (hasPathSum(root->left, targetSum)) return true;
+    if (hasPathSum(root->right, targetSum)) return true;
+
+    // root->left에도 없고 root->right에도 없을 경우
+    return NULL;
+}
