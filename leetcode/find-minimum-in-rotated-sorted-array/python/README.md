a16f2082d3ec4858bcc739429314235584f2969e
Sewon Min <86864525+givemethatsewon@users.noreply.github.com>
2025-02-21 17:17:34 +0900

    [leetcode] find-minimum-in-rotated-sorted-array (Time: 0ms, Memory: 12MB) - by Code-Vault

---
+++ leetcode/find-minimum-in-rotated-sorted-array/python/README.md
@@ -0,0 +1,8 @@
+# Find Minimum In Rotated Sorted Array
+
+[문제 출처](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array)
+
+## 성능
+
+- Time: 0ms
+- Memory: 12MB
---
+++ leetcode/find-minimum-in-rotated-sorted-array/python/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findMin(self, nums):
+        low, high = 0, len(nums) - 1
+        res = min(nums[low], nums[high])
+
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] > res:
+                low = mid + 1
+            else:
+                res = min(nums[mid], res)
+                high = mid - 1
+        
+        return res
+        
